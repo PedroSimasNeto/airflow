5b0ea853227a305a7e90eea86254d992152ed37d
Pedro Simas Neto <pedro.neto@havan.com.br>
2022-07-09 18:59:31 -0300

    Alterações

--- dags/dag_condominio.py
+++ dags/dag_condominio.py
@@ -8,8 +8,10 @@ from airflow.operators.dummy import DummyOperator
 from datetime import datetime, timedelta
 from airflow.models import Variable
 from airflow.decorators import task
+from airflow.settings import TIMEZONE
 from airflow import DAG
 from etl import jobs
+import pendulum
 
 cfg_secrets = Variable.get("administradora_condominios_secret", deserialize_json=True)
 cfg = Variable.get("administradora_condominios", deserialize_json=True)
@@ -30,9 +32,9 @@ def condominios():
 
 
 @task
-def st_relatorio_receitas_despesas(data_execucao):
+def st_relatorio_receitas_despesas(dt):
     parametro = jobs(url=cfg["relatorios"], header=cfg_secrets, database="postgres-datalake")
-    print(data_execucao, type(data_execucao))
+    data_execucao = pendulum.DateTime(dt.year, dt.day, dt.month, tzinfo=TIMEZONE)
     parametro.st_relatorio_receita_despesa(table="st_relatorio_receita_despesa", data_execucao=data_execucao,
                                            intervalo_execucao=cfg["intervalo_execucao"])
     return print("Importado os dados para staging com sucesso!")

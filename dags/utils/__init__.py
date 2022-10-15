b7190778ab4c34cae09ddbaaa8067015a362f4d0
Pedro Simas Neto <pedro.neto@havan.com.br>
2022-10-14 23:59:24 -0300

    fix: Ajustando a função do MySQL

--- dags/utils/__init__.py
+++ dags/utils/__init__.py
@@ -54,11 +54,10 @@ def airflow_buscar_conexao_mysql(database_id):
             """
     config_db = obter_conn_uri(database_id)
     conn = MySQLdb.connect(host=config_db["host"],
-                            port=config_db["port"],
-                            database=config_db["schema"],
-                            user=config_db["user"],
-                            password=config_db["password"],
-                            cursorclass=cursors.DictCursor)
+                           port=config_db["port"],
+                           database=config_db["schema"],
+                           user=config_db["user"],
+                           password=config_db["password"])
     return conn
 
 
@@ -97,8 +96,8 @@ def read_mysql(database_id: str, query: str):
         :param query: Query a ser executada no banco
         :param database_id: Id da database gravada no Airflow
     """
-    with airflow_buscar_conexao_postgres(database_id) as pgsql_conn:
-        with pgsql_conn.cursor() as cursor:
+    with airflow_buscar_conexao_mysql(database_id) as mysql_conn:
+        with mysql_conn.cursor(cursor_factory=cursors.DictCursor) as cursor:
             cursor.execute(query, None)
             return cursor.fetchall()
 

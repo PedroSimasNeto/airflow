2bd013adb91b7fb38a1ac4b693bd756c45444ad0
Pedro Simas Neto <pedro.neto@havan.com.br>
2022-11-02 23:35:49 -0300

    feat: Criado novo metodo para pipeline

--- dags/dag_questor_omie_v01.py
+++ dags/dag_questor_omie_v01.py
@@ -4,8 +4,7 @@ Created on Mon Nov 11 19:00:00 2022
 @author: Pedro Simas Neto
 """
 from airflow import DAG
-from airflow.decorators import task
-# from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator
 from airflow.operators.dummy import DummyOperator
 from airflow.models import Variable
 from datetime import datetime
@@ -19,7 +18,6 @@ default_args = {
 cfg = Variable.get("questor_omie", deserialize_json=True)
 
 
-@task
 def dados_questor(tabelas):
     job = Questor_OMIE(schema="staging", conn="questor", table=tabelas)
     job.datalake()
@@ -35,7 +33,15 @@ with DAG("dag_questor_omie_v01",
     inicio = DummyOperator(task_id="inicio")
     fim = DummyOperator(task_id="fim")
 
-    for t in cfg["tabelas"]:
-        task_questor = dados_questor(tabelas=t)
+    task_questor = []
 
-        inicio >> task_questor >> fim
+    for t in cfg["tabelas"]:
+        task_questor.append(PythonOperator(
+            task_id=f"questor_{t}"
+            python_callable=dados_questor,
+            op_kwargs={
+                "tabelas": f
+            }
+        ))
+
+    inicio >> task_questor >> fim
